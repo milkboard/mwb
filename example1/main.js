42de05fce8ea859bb54dff28c0f39288952bc451
ssohjiro <ssohjiro@gmail.com>
2015-02-14 16:21:11 +0900

    Update main.js
    For example on only client side

--- example1/main.js
+++ example1/main.js
@@ -1,26 +1,14 @@
 requirejs.config({
 	baseUrl: 'js',
 	paths: {
-		'react': '../lib/react/react-with-addons',
-		'backbone': '../lib/backbone/backbone-min',
-		'underscore': '../lib/underscore/underscore-min',
-		'JSXTransformer': '../lib/react/JSXTransformer',
 		'jquery': 'http://code.jquery.com/jquery-1.11.2.min',
-		'jsx': '../lib/react/jsx',
-		'text': '../lib/requirejs/text'
-	},
-	jsx: {
-		fileExtension: '.jsx',
-		harmoney: true,
-		stripTypes: true
+		'mwb': '../../src/mwb'
 	}
 });
 
 require([
 	'jquery',
-	'react',
-	'jsx!../../src/components/example'
+	'mwb'
 ], function($, React, HelloView) {
-	var h = new HelloView();
-	$('body').html(h.render().$el);
+	$( '#mwbArea' ).mwb({ width: 500, height: 400 });
 });
