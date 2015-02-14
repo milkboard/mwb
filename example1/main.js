d89477102a1a08f77707083847ccc7d1e9adb2fb
ssohjiro <ssohjiro@gmail.com>
2015-02-14 15:16:46 +0900

    Update main.js
    Modified paths

--- example1/main.js
+++ example1/main.js
@@ -1,13 +1,13 @@
 requirejs.config({
 	baseUrl: 'js',
 	paths: {
-		'react': 'lib/react/react-with-addons',
-		'backbone': 'lib/backbone/backbone-min',
-		'underscore': 'lib/underscore/underscore-min',
-		'JSXTransformer': 'lib/react/JSXTransformer',
+		'react': '../lib/react/react-with-addons',
+		'backbone': '../lib/backbone/backbone-min',
+		'underscore': '../lib/underscore/underscore-min',
+		'JSXTransformer': '../lib/react/JSXTransformer',
 		'jquery': 'http://code.jquery.com/jquery-1.11.2.min',
-		'jsx': 'lib/react/jsx',
-		'text': 'lib/requirejs/text'
+		'jsx': '../lib/react/jsx',
+		'text': '../lib/requirejs/text'
 	},
 	jsx: {
 		fileExtension: '.jsx',
