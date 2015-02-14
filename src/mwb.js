727540f2d3f0caf8f4020ddf4a5fb2481df8b26c
ssohjiro <ssohjiro@gmail.com>
2015-02-14 16:24:35 +0900

    Update mwb.js

--- src/mwb.js
+++ src/mwb.js
@@ -1,5 +1,12 @@
-define([ 'jquery' ], function( $ ) {
-    $.fn.mwb = function() {
-      // todo
+define([ 'underscore', 'jquery' ], function( _, $ ) {
+    
+    var defaultOptions = {
+        width: 400,
+        height: 300
+    };
+    
+    $.fn.mwb = function( options ) {
+        options = _.extend({}, defaultOptions, options );
+        
     };
 });
