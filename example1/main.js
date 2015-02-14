db33a0b2d2eb2a23a175091af99c6e9425de5c2f
ssohjiro@gmail.com <ssohjiro@gmail.com>
2015-02-14 20:57:37 +0900

    modify path of underscore

--- example1/main.js
+++ example1/main.js
@@ -2,6 +2,7 @@ requirejs.config({
 	baseUrl: 'js',
 	paths: {
 		'jquery': 'http://code.jquery.com/jquery-1.11.2.min',
+		'underscore': '../lib/underscore/underscore-min',
 		'mwb': '../../src/mwb'
 	}
 });
